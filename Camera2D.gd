6d8b6c9660ff5b03bf1f715ff6b054a1b008bfe5
Gerlinde1997 <60713279+Gerlinde1997@users.noreply.github.com>
2021-01-22 15:28:03 +0100

    add Player

---
+++ Camera2D.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+onready var tilemap # LET OP ! = $Tilemap NOG INVULLEN!
+
+func set_camera_limits():
+	var scope = tilemap.get_used_rect()
+	var cellsize = tilemap.cell_size
+	
+	limit_left = scope.position.x * cellsize.x
+	limit_right = scope.end.x * cellsize.x
+	limit_top = scope.position.y * cellsize.y
+	limit_bottom = scope.end.y * cellsize.y
+
+func _ready():
+	set_camera_limits()
---
+++ Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var facingDir = Vector2()
+
+var moveSpeed = 100
+
+func play_animation(anim_name):
+	if $AnimatedSprite.animation != anim_name:
+		$AnimatedSprite.play(anim_name)
+
+func manage_animations():
+	if velocity.x > 0:
+		play_animation("MoveRight")
+	elif velocity.x < 0:
+		play_animation("MoveLeft")
+	elif velocity.y < 0:
+		play_animation("MoveUp")
+	elif velocity.y > 0:
+		play_animation("MoveDown")
+	elif facingDir.x == 1:
+		play_animation("IdleRight")
+	elif facingDir.x == -1:
+		play_animation("IdleLeft")
+	elif facingDir.y == 1:
+		play_animation("IdleDown")
+	elif facingDir.y == -1:
+		play_animation("IdleUp")
+
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -1
+		facingDir = Vector2(0, -1)
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		facingDir = Vector2(0, 1)
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -1
+		facingDir = Vector2(-1, 0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		facingDir = Vector2(1, 0)
+	
+	velocity = velocity.normalized()
+	
+	move_and_slide(velocity * moveSpeed)
+	manage_animations()
